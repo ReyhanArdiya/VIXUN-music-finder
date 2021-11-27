83a7614e75434b3e6e141b65d1c5710e18ae0361
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-11-27 20:59:48 +0700

    Add description for mediaQuery object

--- scripts/mediaQueries.js
+++ scripts/mediaQueries.js
@@ -1,3 +1,6 @@
+/**
+ * Use to store mediaQueryList objects for this website's media queries that needs behavior based on breakpoints.
+ */
 const mediaQuery = {
 	medium: window.matchMedia("(min-width: 48em")
 };
