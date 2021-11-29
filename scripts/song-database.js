d47fbe816a6a75062aa7860923549b06d5f1f3cf
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-11-29 19:57:48 +0700

    Add Song class

---
+++ scripts/song-database.js
@@ -0,0 +1,25 @@
+class Song {
+	/**
+	 *
+	 * @param {string} title
+	 * @param {string} artist
+	 * @param {string} album
+	 * @param {string} genre
+	 * @param {number} year
+	 * @param {number} price
+	 * @param {string} coverURL
+	 * @param {string} fileURL
+	 * @param {boolean} isOnSale
+	 */
+	constructor(title, artist, album, genre, year, price, coverURL, fileURL, isOnSale = false) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.genre = genre;
+		this.year = year;
+		this.price = price;
+		this.coverURL = coverURL;
+		this.fileURL = fileURL;
+		this.isOnSale = isOnSale;
+	}
+}
