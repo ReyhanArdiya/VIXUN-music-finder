d19f7a2ada804ceb36bba651c3415e6a3b89d8b7
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-03 22:35:28 +0700

    Add "use strict" to all JS files

--- scripts/media-queries.js
+++ scripts/media-queries.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * Use to store mediaQueryList objects for this website's media queries that needs behavior based on breakpoints.
  */
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,3 +1,4 @@
+"use strict";
 const navbarMain = {
 	menu: document.querySelector("#navbar-main"),
 	content: {
