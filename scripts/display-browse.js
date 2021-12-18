ad3a42d8a06d75305d8357bd923598072937ccbb
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-18 20:50:34 +0700

    Add method to append category icons

--- scripts/display-browse.js
+++ scripts/display-browse.js
@@ -1,5 +1,17 @@
 const displayBrowse = {
-	categories: document.querySelector("#display-browse-categoires"),
+	categories: {
+		container: document.querySelector("#display-browse-categories"),
+		iconTemplate: document.querySelector("#icon-category-template"),
+
+		addIcon(imgURL, label = "Type") {
+			/**@type {HTMLDivElement}*/
+			const icon = this.iconTemplate.content.firstElementChild.cloneNode(true);
+			icon.lastElementChild.innerText = label;
+			icon.firstElementChild.style.background = `url(${imgURL})`;
+			console.log(icon);
+			this.container.append(icon);
+		}
+	},
 	searchBar: document.querySelector("#display-browse-searchbar"),
 	songs: document.querySelector("#display-browse-songs")
 };
