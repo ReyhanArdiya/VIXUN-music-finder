07b9fd1089c56a7516a5baee6bde406ddd9030d5
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-18 21:02:31 +0700

    Use img for icon-category picture

--- scripts/display-browse.js
+++ scripts/display-browse.js
@@ -3,12 +3,14 @@ const displayBrowse = {
 		container: document.querySelector("#display-browse-categories"),
 		iconTemplate: document.querySelector("#icon-category-template"),
 
-		addIcon(imgURL, label = "Type") {
+		addIcon(label = "Type", imgURL = "https://placekitten.com/100/100") {
 			/**@type {HTMLDivElement}*/
 			const icon = this.iconTemplate.content.firstElementChild.cloneNode(true);
-			label = `${label[0].toUpperCase()}${label.slice(1).toLowerCase()}`;
+			if (label.length) {
+				label = `${label[0].toUpperCase()}${label.slice(1).toLowerCase()}`;
+			}
 			icon.lastElementChild.innerText = label;
-			icon.firstElementChild.style.background = `url(${imgURL})`;
+			icon.firstElementChild.src = imgURL;
 			console.log(icon);
 			this.container.append(icon);
 		}
@@ -16,3 +18,25 @@ const displayBrowse = {
 	searchBar: document.querySelector("#display-browse-searchbar"),
 	songs: document.querySelector("#display-browse-songs")
 };
+
+// DBG
+displayBrowse.categories.addIcon("Pop", "https://placekitten.com/500/500");
+displayBrowse.categories.addIcon("Shoegaze", "https://placekitten.com/400/400");
+displayBrowse.categories.addIcon("Rock", "https://placekitten.com/300/300");
+displayBrowse.categories.addIcon("Lo-Fi", "https://placekitten.com/200/200");
+displayBrowse.categories.addIcon("Indie", "https://placekitten.com/100/100");
+displayBrowse.categories.addIcon("Pop", "https://placekitten.com/500/500");
+displayBrowse.categories.addIcon("Shoegaze", "https://placekitten.com/400/400");
+displayBrowse.categories.addIcon("Rock", "https://placekitten.com/300/300");
+displayBrowse.categories.addIcon("Lo-Fi", "https://placekitten.com/200/200");
+displayBrowse.categories.addIcon("Indie", "https://placekitten.com/100/100");
+displayBrowse.categories.addIcon("Pop", "https://placekitten.com/500/500");
+displayBrowse.categories.addIcon("Shoegaze", "https://placekitten.com/400/400");
+displayBrowse.categories.addIcon("Rock", "https://placekitten.com/300/300");
+displayBrowse.categories.addIcon("Lo-Fi", "https://placekitten.com/200/200");
+displayBrowse.categories.addIcon("Indie", "https://placekitten.com/100/100");
+displayBrowse.categories.addIcon("Pop", "https://placekitten.com/500/500");
+displayBrowse.categories.addIcon("Shoegaze", "https://placekitten.com/400/400");
+displayBrowse.categories.addIcon("Rock", "https://placekitten.com/300/300");
+displayBrowse.categories.addIcon("Lo-Fi", "https://placekitten.com/200/200");
+displayBrowse.categories.addIcon("Indie", "https://placekitten.com/100/100");
