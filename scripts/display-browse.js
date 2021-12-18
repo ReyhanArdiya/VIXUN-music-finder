dd1e8343df2a94bbd91036344a550743c3120946
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-18 20:44:02 +0700

    Make displayBrowse object

---
+++ scripts/display-browse.js
@@ -0,0 +1,5 @@
+const displayBrowse = {
+	categories: document.querySelector("#display-browse-categoires"),
+	searchBar: document.querySelector("#display-browse-searchbar"),
+	songs: document.querySelector("#display-browse-songs")
+};
