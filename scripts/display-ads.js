b5200f585cfc098149bca1c81dcb299080819502
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-19 21:45:21 +0700

    Add logic in displayAds to track currently displayed ad

--- scripts/display-ads.js
+++ scripts/display-ads.js
@@ -1,6 +1,12 @@
 const displayAds = {
 	contentContainer: document.querySelector("#display-ads-content"),
+	currentAds: document.querySelector("#display-ads-content").getElementsByTagName("img"),
 	itemTemplate: document.querySelector("#display-ads-item-template").content.firstElementChild,
+
+	_currentDisplayedAd: 1,
+	get currentDisplayedAd() {
+		return this._currentDisplayedAd;
+	},
 
 	appendNewAd(imgSource) {
 		const adItem = this.itemTemplate.cloneNode(true);
@@ -24,6 +30,29 @@ const displayAds = {
 
 	scrollToAd(adNumber) {
 		this.contentContainer.scrollLeft = this.getAdObj(adNumber).offsetLeft;
+	},
+
+	/**
+	 * Only call this method ONCE to track {@link displayAds._currentDisplayedAd}.
+	 */
+	addObserverForScrollLoc() {
+		const observer = new IntersectionObserver(
+			entries => {
+				for (let i = 0; i < this.currentAds.length; i++) {
+					// True if an ad reference value (this.currentAds[i]) is the same as the reported observed target (entries[0].target). When an observed element (i.e. an ad) is intersecting, it is going to call this callback and pass the element that is intersecting right now AS THE ONLY ITEM in the entries parameter. Since we only get one item in entries, we just need to compare that reported element with all the current ads.
+					if (this.currentAds[i] === entries[0].target) {
+						this._currentDisplayedAd = i + 1;
+						break;
+					}
+				}
+			},
+			{ root: this.contentContainer, threshold: 1 }
+		);
+
+		// Observe all current ads
+		for (let i = 0; i < this.currentAds.length; i++) {
+			observer.observe(this.currentAds[i]);
+		}
 	}
 };
 
@@ -35,4 +64,5 @@ displayAds.appendNewAd("https://placekitten.com/400/300");
 displayAds.appendNewAd("https://placekitten.com/300/400");
 displayAds.appendNewAd("https://placekitten.com/500/500");
 
-displayAds.autoScroll(5000);
+displayAds.addObserverForScrollLoc();
+// displayAds.autoScroll(5000);
