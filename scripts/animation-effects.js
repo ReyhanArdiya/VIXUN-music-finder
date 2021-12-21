4446673ebbb0091e91866d92ee0e221764db3e00
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-21 08:39:10 +0700

    Extract detectScrollDirectionMaker to new object

--- scripts/animation-effects.js
+++ scripts/animation-effects.js
@@ -1,39 +1,14 @@
 const animationEffects = {
 	/**
-	 * This is a factory function which returns a function that will automatically detect the scrolling direction of {@link scrollingEl} both for vertical and horizontal scrolling or based on the {@link mediaQuery.medium} matches property.
-	 * @param {HTMLElement} scrollingEl Any scrolling element to detect the direction of.
-	 * @param {"horizontal" | "vertical" | "breakpointMedium"} trackDirection Track scrolling direction horizontally, vertically or based on {@link mediaQuery.medium} matches property. If "breakpointMedium" is chosen, if the matches property is false it will detect vertical scrolling and return either "down" or "up". If matches property is true, it will detect horizontal scrolling direction and return either "right" or "left".
-	 * @returns {() => "up" | "right" | "down" | "left"}
-	 */
-	detectScrollDirectionMaker(scrollingEl, trackDirection) {
-		let prevScrollPos = 0;
-		let currentScrollPos = 0;
-
-		return () => {
-			let direction = "unknown";
-			if (trackDirection === "horizontal" || (trackDirection === "breakpointMedium" && mediaQuery.medium.matches)) {
-				currentScrollPos = scrollingEl.scrollLeft;
-				direction = currentScrollPos >= prevScrollPos ? "right" : "left";
-			} else if (trackDirection === "vertical" || (trackDirection === "breakpointMedium" && !mediaQuery.medium.matches)) {
-				currentScrollPos = scrollingEl.scrollTop;
-				direction = currentScrollPos >= prevScrollPos ? "down" : "up";
-			}
-
-			prevScrollPos = currentScrollPos;
-			return direction;
-		};
-	},
-
-	/**
 	 * Adds parallax effect to an element based on a scrollable element.
 	 * @param {HTMLElement} scrollingEl Any element that is scrollable, can be the container of the {@link parallaxedItemsArr} or any scrollable element.
 	 * @param {HTMLElement[]} parallaxedItemsArr Array of any HTMLObject reference that will be parallaxed.
 	 * @param {HTMLElement[]} speed Number to configure {@link parallaxedItemsArr}'s parallax speed.
-	 * @param {"horizontal" | "vertical" | "breakpointMedium"} trackDirection String to set which scroll direction on {@link scrollingEl} to track and activate the parallax effect. The way it works is by passing this argument to {@link animationEffects.detectScrollDirectionMaker} and calling the returned function to get the tracked direction and move the parallaxed items based on the direction.
+	 * @param {"horizontal" | "vertical" | "breakpointMedium"} trackDirection String to set which scroll direction on {@link scrollingEl} to track and activate the parallax effect. The way it works is by passing this argument to {@link movementDetector.detectScrollDirectionMaker} and calling the returned function to get the tracked direction and move the parallaxed items based on the direction.
 	 */
 	addParallax(scrollingEl, parallaxedItemsArr, speed, trackDirection) {
 		let currentTranslateVal = 0;
-		const detectScroll = animationEffects.detectScrollDirectionMaker(scrollingEl, trackDirection);
+		const detectScroll = movementDetector.detectScrollDirectionMaker(scrollingEl, trackDirection);
 
 		scrollingEl.addEventListener(
 			"scroll",
@@ -69,9 +44,36 @@ const animationEffects = {
 					}
 				};
 
-			moveCircles(detectScroll());
+				moveCircles(detectScroll());
 			},
 			{ passive: true }
 		);
+	}
+};
+
+const movementDetector = {
+	/**
+	 * This is a factory function which returns a function that will automatically detect the scrolling direction of {@link scrollingEl} both for vertical and horizontal scrolling or based on the {@link mediaQuery.medium} matches property.
+	 * @param {HTMLElement} scrollingEl Any scrolling element to detect the direction of.
+	 * @param {"horizontal" | "vertical" | "breakpointMedium"} trackDirection Track scrolling direction horizontally, vertically or based on {@link mediaQuery.medium} matches property. If "breakpointMedium" is chosen, if the matches property is false it will detect vertical scrolling and return either "down" or "up". If matches property is true, it will detect horizontal scrolling direction and return either "right" or "left".
+	 * @returns {() => "up" | "right" | "down" | "left"}
+	 */
+	detectScrollDirectionMaker(scrollingEl, trackDirection) {
+		let prevScrollPos = 0;
+		let currentScrollPos = 0;
+
+		return () => {
+			let direction = "unknown";
+			if (trackDirection === "horizontal" || (trackDirection === "breakpointMedium" && mediaQuery.medium.matches)) {
+				currentScrollPos = scrollingEl.scrollLeft;
+				direction = currentScrollPos >= prevScrollPos ? "right" : "left";
+			} else if (trackDirection === "vertical" || (trackDirection === "breakpointMedium" && !mediaQuery.medium.matches)) {
+				currentScrollPos = scrollingEl.scrollTop;
+				direction = currentScrollPos >= prevScrollPos ? "down" : "up";
+			}
+
+			prevScrollPos = currentScrollPos;
+			return direction;
+		};
 	}
 };
