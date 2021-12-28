056c844c5196081c26502936696e67be1f50a959
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2021-12-28 23:02:28 +0700

    Reroute public files

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const port = 9000;
 
 const app = express();
 app.set("view engine", "ejs");
-app.set("views", join(__dirname, "/views"));
-
-const port = 9000;
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "public")));
 
 // TODO learn and use Route objects
 // Set the routes for homepage
