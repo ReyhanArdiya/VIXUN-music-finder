c0877dc4285dc888ac2fba46e56b3662ee034957
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-12 20:32:32 +0700

    Create `songSchema`

---
+++ models/song.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+	album : {
+		type     : String,
+		required : true
+	},
+	artist : {
+		type     : String,
+		required : true
+	},
+	coverURL : {
+		type     : String,
+		required : true
+	},
+	genre : {
+		type     : String,
+		required : true
+	},
+	isOnSale : {
+		type    : Boolean,
+		default : false
+	},
+	links : {
+		spotify     : String,
+		deezer      : String,
+		amazonMusic : String,
+	},
+	priceUSD : {
+		type     : Number,
+		required : true
+	},
+	title : {
+		type     : String,
+		required : true
+	},
+	year : {
+		type     : Number,
+		required : true
+	},
+});
