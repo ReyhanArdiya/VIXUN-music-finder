95b44ce8a6c6a50a1899e93cb58b833f40476e51
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-15 09:48:07 +0700

    Rename `routes` folder to `routers`

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import "./vixunDB.js";
 import express from "express";
 import { fileURLToPath } from "url";
-import routerHome from "./routes/home.js";
+import routerHome from "./routers/home.js";
 import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
