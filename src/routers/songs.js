a513fff4af600c1efcee891e9238597612c510e9
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-23 14:56:34 +0700

    Add `songsRouter` route handlers

--- src/routers/songs.js
+++ src/routers/songs.js
@@ -1,6 +1,32 @@
+import Song from "../models/song.js";
+import { aggregatorAPIs } from "../database/song-aggregator/index.js";
 import express from "express";
+import requestSongs from "../database/index.js";
 
 const songsRouter = express.Router();
+songsRouter.use(express.urlencoded({ extended : true }));
 
+songsRouter.get("/", async (req, res, next) => {
+	try {
+		let songs;
+		if (req.query?.q) {
+			songs = await requestSongs(req.query.q);
+		} else {
+			songs = await Song.find().limit(10);
+		}
+		res.send(songs);
+	} catch (err) {
+		next(err);
+	}
+});
+
+songsRouter.get("/top", async (req, res, next) => {
+	try {
+		const topSongs = await aggregatorAPIs.deezer.searchDeezerChart("/tracks");
+		res.send(topSongs);
+	} catch (err) {
+		next(err);
+	}
+});
 
 export default songsRouter;
