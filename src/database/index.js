2b22c3880fa7b528dd829c1b11a96b7c5270b5fd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-23 10:28:21 +0700

    Implement `requestSongs` function

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
+import Song from "../models/song.js";
+import aggregateAndSave from "./utils/aggregate-save.js";
 import { config } from "dotenv";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
+import newPageRandomUA from "./utils/user-agent.js";
+import puppeteer from "puppeteer";
 import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,3 +19,47 @@ try {
 } catch (err) {
 	console.log(`Error! Can't connect to ${mongoDatabase}!🍂`, err);
 }
+
+const browser = await puppeteer.launch();
+
+/**
+ * This function will aggregate the song's data found from querying `q` and
+ * saves it to the DB if no duplicate was detected. Whether this function saves
+ * the found song or not, it will always return an array of song documents from
+ * the results of querying using `q`.
+ *
+ * @param {string} q
+ * A flexible case insensitive string that will be used when aggregating
+ * the song's data and querying the song database. Examples:
+ * 1. "The hours beach house bloom"
+ * 2. "song depression space beach cherry house"
+ * 3. "CHROM gAgA lAd 11 bOy suMMer"
+ *
+ * @param {import("puppeteer").Page} page
+ * An optional `puppeteer.Page`. If omitted it will use this module's own page.
+ *
+ * @returns A promise that resolves into the results of querying the database
+ * sorted from the most relevant results.
+ *
+ * @example
+ * Only save this new song if the entry doesn't exist in the database. Whether
+ * it was saved or not, this function will always return the results of
+ * the query.
+ * ```js
+ * console.log(await requestSongs("the hours beach house bloom"));
+ * ```
+ */
+const requestSongs = async (q, page = newPageRandomUA(browser)) => {
+	try {
+		await aggregateAndSave(q, page);
+	} catch (err) { /* nothing */ }
+
+	return await Song.querySongs(q);
+};
+
+export default requestSongs;
+
+// DBG some requestSongs tests
+// console.log(await requestSongs("The hours beach house bloom"));
+// console.log(await requestSongs("song depression space beach cherry house"));
+// console.log(await requestSongs("CHROM gAgA lAd 11 bOy suMMer"));
