3d0643ea42d1eeea42a4d1e212837b444afd8098
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-24 15:22:36 +0700

    Repurpose category icons to display artists image

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,8 +1,8 @@
+import Song from "../models/song.js";
 import { config } from "dotenv";
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import Song from "../models/song.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,39 +10,6 @@ const __dirname = dirname(__filename);
 config({ path : join(__dirname, "..", "..", "process.env") });
 
 const homeRouter = express.Router();
-
-class CategoryIcon {
-	 constructor(label = "Type", imgURL = "https://placekitten.com/100/100") {
-
-		// Change the label's casing
-		if (label.length) {
-			label = `${label[0].toUpperCase()}${label.slice(1)
-				.toLowerCase()}`;
-		}
-
-		this.label = label;
-		this.imgURL = imgURL;
-	}
-}
-// TODO do i need to delete this? since my apis dont have ways to get the genre :(
-const categoryIcons =
-[
-	new CategoryIcon("Pop", "https://placekitten.com/500/500"),
-	new CategoryIcon("Shoegaze", "https://placekitten.com/400/400"),
-	new CategoryIcon("Rock", "https://placekitten.com/300/300"),
-	new CategoryIcon("Lo-Fi", "https://placekitten.com/200/200"),
-	new CategoryIcon("Indie", "https://placekitten.com/100/100"),
-	new CategoryIcon("Pop", "https://placekitten.com/500/500"),
-	new CategoryIcon("Shoegaze", "https://placekitten.com/400/400"),
-	new CategoryIcon("Rock", "https://placekitten.com/300/300"),
-	new CategoryIcon("Lo-Fi", "https://placekitten.com/200/200"),
-	new CategoryIcon("Indie", "https://placekitten.com/100/100"),
-	new CategoryIcon("Pop", "https://placekitten.com/500/500"),
-	new CategoryIcon("Shoegaze", "https://placekitten.com/400/400"),
-	new CategoryIcon("Rock", "https://placekitten.com/300/300"),
-	new CategoryIcon("Lo-Fi", "https://placekitten.com/200/200"),
-	new CategoryIcon("Indie", "https://placekitten.com/100/100"),
-];
 
 homeRouter.get("/", async (req, res, next) => {
 	try {
@@ -53,11 +20,14 @@ homeRouter.get("/", async (req, res, next) => {
 
 		const count = await Song.estimatedDocumentCount();
 		const random = Math.floor(Math.random() * count);
-		const songs = await Song.find().skip(random).limit(15);
+		const songs = await Song.find()
+			                     .skip(random)
+			                     .limit(15);
+		const artists = new Set(songs.map(song => song.artistImage));
 
 		res.render("home", {
 			DOMAIN : process.env.DOMAIN,
-			categoryIcons,
+			artists,
 			navbarLink,
 			songs
 		});
