4972cd84c3885c8378f8899c80aef1d12de045bf
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-25 10:29:40 +0700

    Improve top hits to append top albums and artists

--- src/public/home/scripts/top-hits.js
+++ src/public/home/scripts/top-hits.js
@@ -6,8 +6,37 @@ const { animation: { animationEffects } } = core;
 // Add song cards from database to top hits grid
 window.addEventListener("load", async function() {
 	try {
-		const topHits = (await axios.get("/songs/top")).data?.data;
-		const topHitsExt = topHits.map(s => {
+		const topHits = (await axios.get("/songs/top")).data;
+		const {
+			albums: { data: albums },
+			artists: { data: artists },
+			tracks: { data: tracks }
+		} = topHits;
+
+		const albumsExt = albums.map(a => {
+			const {
+				cover_medium: image,
+				artist: { name: artist },
+				title
+			} = a;
+
+			return {
+				image,
+				query : `${title} ${artist}`
+			};
+		});
+		const artistsExt = artists.map(a => {
+			const {
+				picture_medium: image,
+				name: artist
+			} = a;
+
+			return {
+				image,
+				query : `${artist}`
+			};
+		});
+		const tracksExt = tracks.map(s => {
 			const {
 				album  : { cover_big : image, title: album },
 				title,
@@ -19,7 +48,10 @@ window.addEventListener("load", async function() {
 				query : `${title} ${artist} ${album}`
 			};
 		});
-		displayTopHits.addDatabaseSongs(topHitsExt);
+
+		displayTopHits.addDatabaseSongs(
+			[ ...tracksExt, ...albumsExt, ...artistsExt ]
+		);
 	} catch (err) {
 		console.error(err);
 	}
