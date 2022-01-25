450d68205e24c0785fb8f0c6c2ea3a1f210bb39d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-25 10:14:28 +0700

    Implement top hits logic for adding and clicking

--- src/public/home/scripts/top-hits.js
+++ src/public/home/scripts/top-hits.js
@@ -1,10 +1,29 @@
 import core from "../../common/scripts/index.js";
 import { displayTopHits } from "./display-objects.js";
 
-const { animation: { animationEffects }, localSongDatabase } = core;
+const { animation: { animationEffects } } = core;
 
 // Add song cards from database to top hits grid
-displayTopHits.addDatabaseSongs(localSongDatabase);
+window.addEventListener("load", async function() {
+	try {
+		const topHits = (await axios.get("/songs/top")).data?.data;
+		const topHitsExt = topHits.map(s => {
+			const {
+				album  : { cover_big : image, title: album },
+				title,
+				artist : { name : artist }
+			} = s;
+
+			return {
+				image,
+				query : `${title} ${artist} ${album}`
+			};
+		});
+		displayTopHits.addDatabaseSongs(topHitsExt);
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 // Add parallax to display top hits' circle decorations.
 animationEffects.addParallax(
