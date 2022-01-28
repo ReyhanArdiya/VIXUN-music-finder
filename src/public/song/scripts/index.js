a769cf17d342946487ac58e428844f12b5d0c523
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-28 22:16:43 +0700

    Implement share button logic

--- src/public/song/scripts/index.js
+++ src/public/song/scripts/index.js
@@ -3,12 +3,14 @@ import core from "../../common/scripts/index.js";
 
 const { animation: { animationEffects } } = core;
 
+// Setting up main navbar
 const navbarMain = checkNavbar();
 navbarMain.dropdown.text[0].firstElementChild.innerText = "SONG";
 navbarMain.dropdown.text[0].firstElementChild.href = "#page-header";
 navbarMain.dropdown.text[1].firstElementChild.innerText = "COMMENTS";
 navbarMain.dropdown.text[1].firstElementChild.href = "#display-comments";
 
+// Add parallaz for circle decos
 const displayCommentsCircles = document.querySelectorAll("#display-comments svg[class*='circle-decoration']");
 animationEffects.addParallax(
 	document.querySelector("#display-comments-render"),
@@ -18,6 +20,7 @@ animationEffects.addParallax(
 	true
 );
 
+// Add playback logic to song image
 const audio = document.querySelector("#song-image audio");
 const songPlay = document.getElementById("song-play");
 const songPause = document.getElementById("song-pause");
@@ -37,4 +40,11 @@ document.querySelector("#song-image").addEventListener("click", function() {
 audio.addEventListener("ended", function() {
 	songPlay.style.display = "block";
 	songPause.style.display = "none";
+});
+
+// Share button logic
+const share = document.getElementById("share");
+const url = window.location.href;
+share.addEventListener("click", async function() {
+	navigator.share({ url });
 });
