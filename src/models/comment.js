1db9082d40fe55d0f875c80c7bb94748f73f2298
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-28 23:52:30 +0700

    Implement post save middleware for `CommentSchema`

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,3 +1,4 @@
+import Song from "./song.js";
 import mongoose from "mongoose";
 
 /**
@@ -23,6 +24,13 @@ const CommentSchema = new mongoose.Schema({
 		type     : mongoose.Types.ObjectId
 	},
 }, { strict : "throw" });
+
+CommentSchema.post("save", async function(comment) {
+	console.log(comment);
+	const song = await Song.findById(comment.song);
+	song.comments.push(comment);
+	song.save();
+});
 
 class CommentSchemaMethods {}
 CommentSchema.loadClass(CommentSchemaMethods);
