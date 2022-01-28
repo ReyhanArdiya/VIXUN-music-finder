0007264a03f23010f50655e64ccd3a2fbbc25854
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-28 22:52:01 +0700

    Create typedef for `Comment` model

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+/**
+ * Type for instance of {@link Comment}.
+ *
+ * @typedef {import("mongoose").Document & {
+ * song: import("./song.js").SongDocument
+ * text: string
+ * user: import("./user.js").UserDocument
+ * }} CommentDocument
+ */
 const CommentSchema = new mongoose.Schema({
 	song : {
 		required : true,
