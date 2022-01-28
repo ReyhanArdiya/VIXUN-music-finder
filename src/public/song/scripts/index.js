bb916c1c6077193276330eaef2914c7040e13802
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-28 14:23:33 +0700

    Add parallax effect for comments circle decorations

--- src/public/song/scripts/index.js
+++ src/public/song/scripts/index.js
@@ -1,7 +1,19 @@
 import checkNavbar from "../../common/scripts/navbar-main.js";
+import core from "../../common/scripts/index.js";
+
+const { animation: { animationEffects } } = core;
 
 const navbarMain = checkNavbar();
 navbarMain.dropdown.text[0].firstElementChild.innerText = "SONG";
 navbarMain.dropdown.text[0].firstElementChild.href = "#page-header";
 navbarMain.dropdown.text[1].firstElementChild.innerText = "COMMENTS";
 navbarMain.dropdown.text[1].firstElementChild.href = "#display-comments";
+
+const displayCommentsCircles = document.querySelectorAll("#display-comments svg[class*='circle-decoration']");
+animationEffects.addParallax(
+	document.querySelector("#display-comments-render"),
+	displayCommentsCircles,
+	0.1,
+	"vertical",
+	true
+);
