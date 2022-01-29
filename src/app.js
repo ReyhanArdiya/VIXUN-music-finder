d178e9fe21b1da75304a3452f16597736dc9604e
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-29 19:31:20 +0700

    Setup session and passport

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+import User from "./models/user.js";
 import { config } from "dotenv";
 import ejsEngine from "ejs-mate";
 import express from "express";
 import { fileURLToPath } from "url";
 import homeRouter from "./routers/home.js";
 import mongoose from "mongoose";
+import passport from "passport";
+import session from "express-session";
 import songsRouter from "./routers/songs.js";
 import { dirname, join } from "path";
 
@@ -12,6 +15,7 @@ const __dirname = dirname(__filename);
 
 config({ path : join(__dirname, "..", "process.env") });
 
+// Mongoose stuff
 const mongoDatabase = process.env.MONGODB;
 try {
 	await mongoose.connect(`mongodb://localhost:27017/${mongoDatabase}`);
@@ -20,6 +24,7 @@ try {
 	console.log(`Error! Can't connect to ${mongoDatabase}!🍂`, err);
 }
 
+// Express stuff
 const port = process.env.PORT;
 const app = express();
 app.set("view engine", "ejs");
@@ -27,6 +32,20 @@ app.engine("ejs", ejsEngine);
 app.set("views", join(__dirname, "views"));
 
 app.use(express.static(join(__dirname, "public")));
+app.use(session({
+	cookie            : { maxAge : 1000 * 60 * 60 * 24 * 7 },
+	resave            : false,
+	saveUninitialized : true,
+	secret            : process.env.SESSION_SECRET,
+}));
+
+// Passport stuff
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Request logger
 app.use((req, res, next) => {
