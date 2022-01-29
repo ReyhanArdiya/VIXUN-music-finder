63968dc6c1a89de0e67461ce33864442ab3e5a10
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-01-29 19:51:16 +0700

    Create views for register and login

--- src/routers/users.js
+++ src/routers/users.js
@@ -5,20 +5,35 @@ const authRouter = express.Router({ mergeParams : true });
 
 authRouter.use(express.urlencoded({ extended : true }));
 
-authRouter.route("/")
-	.post(async (req, res, next) => {
-		try {
-			const { email, username, password } = req.body;
-			const user = new User({
-				email,
-				username,
-			});
-			const newUser = await User.register(user, password);
-			req.login(newUser, err => err && console.error(err));
-			res.redirect("/");
-		} catch (err) {
-			next(err);
-		}
-	});
+/*
+POST http://localhost:9000/auth
+Content-Type: application/x-www-form-urlencoded
+
+email=elys@gmail.com
+&username=elys
+&password=elys
+*/
+authRouter.post("/", async (req, res, next) => {
+	try {
+		const { email, username, password } = req.body;
+		const user = new User({
+			email,
+			username,
+		});
+		const newUser = await User.register(user, password);
+		req.login(newUser, err => err && console.error(err));
+		res.redirect("/");
+	} catch (err) {
+		next(err);
+	}
+});
+
+authRouter.get("/register", (req, res) => {
+	res.render("auth/register");
+});
+
+authRouter.get("/login", (req, res) => {
+	res.render("auth/login");
+});
 
 export default authRouter;
