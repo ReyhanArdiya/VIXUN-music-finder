b50f15ec9a37a4b94c6e19f680b5b6864f847a17
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-01 20:32:30 +0700

    Change `mongoose.Types` to `mongoose.Schema.Types`

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ import passportLocalMongoose from "passport-local-mongoose";
 const UserSchema = new mongoose.Schema({
 	comments : [ {
 		ref  : "Comment",
-		type : mongoose.Types.ObjectId
+		type : mongoose.Schema.Types.ObjectId
 	} ],
 	email : {
 		required : true,
