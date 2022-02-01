27c7da90d8f33ce4048dc924ec3d85e319a68498
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-01 21:09:20 +0700

    Implement `addFavorite`

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,4 @@
+import Song from "../models/song.js";
 import User from "../models/user.js";
 
 const renderHome = async (req, res) => {
@@ -10,7 +11,14 @@ const renderHome = async (req, res) => {
 
 const addFavorite = async (req, res, next) => {
 	try {
-
+		const { id } = req.params;
+		const song = await Song.findById(id);
+		User.findByIdAndUpdate(
+			req.user._id,
+			{ $addToSet : { favorites : song } },
+			{ new : true }
+		);
+		res.end();
 	} catch (err) {
 		next(err);
 	}
