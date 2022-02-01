a34326da852e03d4590d57cead5dd0f7238f9c4d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-01 21:09:40 +0700

    Implement route and ajax call to add favorite

--- src/routers/favorite.js
+++ src/routers/favorite.js
@@ -1,6 +1,8 @@
 import express from "express";
+import userController from "../controllers/user.js";
 
 const favoriteRouter = express.Router({ mergeParams : true });
 
+favoriteRouter.post("/", userController.addFavorite);
 
 export default favoriteRouter;
