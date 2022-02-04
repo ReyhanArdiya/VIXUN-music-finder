50e0e4e316a90c8e886b4505f0cb701d8163c232
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-04 17:28:51 +0700

    Add size limit for profile picture

--- src/public/user/scripts/profile.js
+++ src/public/user/scripts/profile.js
@@ -1,14 +1,16 @@
 const userProfile = document.getElementById("user-image");
 const profileFormInput = document.getElementById("form-profile-input");
 
-// TODO add size limit
-// TODO check what happens if upload not right format
 userProfile.addEventListener("click", function() {
 	profileFormInput.click();
 });
 
 profileFormInput.addEventListener("change", async function() {
-	if (profileFormInput.value) {
+	// TODO check what happens if upload not right format
+	if (profileFormInput.files[0].size > 1_000_000) {
+		// TODO style this i guess
+		alert("Image can't be larger than 1mb!");
+	} else if (profileFormInput.files.length) {
 		const imageData = new FormData();
 		imageData.append("img", profileFormInput.files[0]);
 		// TODO use loading screen here maybe
@@ -20,7 +22,6 @@ profileFormInput.addEventListener("change", async function() {
 				timeout : 10000
 			}
 		);
-		console.dir(profileFormInput);
 		userProfile.src = newProfile.data.path.replace("/upload", "/upload/w_250");
 	}
 });
