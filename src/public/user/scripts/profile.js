cff334d5583726d413ddcf9ff351af24ea16f2e7
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-02-05 09:23:45 +0700

    Add progress bar for profile picture upload

--- src/public/user/scripts/profile.js
+++ src/public/user/scripts/profile.js
@@ -1,4 +1,4 @@
-const userProfile = document.getElementById("user-image");
+const userProfile = document.querySelector("#user-image img");
 const profileFormInput = document.getElementById("form-profile-input");
 
 userProfile.addEventListener("click", function() {
@@ -6,6 +6,21 @@ userProfile.addEventListener("click", function() {
 });
 
 profileFormInput.addEventListener("change", async function() {
+	const progressBar = new ProgressBar.Circle(
+		userProfile.parentElement,
+		{
+			color    : "#ff0000",
+			duration : 3000,
+			easing   : "easeInOut",
+			from     : { color : "#ff0000" },
+			step(state, circle) {
+				circle.path.setAttribute("stroke", state.color);
+			},
+			strokeWidth : 5,
+			to          : { color : "#a129ff" },
+		}
+	);
+
 	try {
 		if (profileFormInput.files[0].size > 1_000_000) {
 			// TODO style this i guess
@@ -13,7 +28,8 @@ profileFormInput.addEventListener("change", async function() {
 		} else if (profileFormInput.files.length) {
 			const imageData = new FormData();
 			imageData.append("img", profileFormInput.files[0]);
-			// TODO use loading screen here maybe
+
+			progressBar.animate(1);
 			const newProfile = await axios.post(
 				"/user/profile",
 				imageData,
@@ -22,9 +38,12 @@ profileFormInput.addEventListener("change", async function() {
 					timeout : 10000
 				}
 			);
+
 			userProfile.src = newProfile.data.path.replace("/upload", "/upload/w_250");
 		}
 	} catch (err) {
 		alert(err.response?.data);
 	}
+
+	progressBar.destroy();
 });
